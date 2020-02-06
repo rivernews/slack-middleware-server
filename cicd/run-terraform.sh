e69ee3d8ae2d2b182894db4cdb66fde1a60fa620
Shaung Cheng <shaungc@umich.edu>
2020-02-06 13:56:19 -0800

    fix terraform command

--- cicd/run-terraform.sh
+++ cicd/run-terraform.sh
@@ -11,9 +11,9 @@
 set +o history
 
 docker run --rm -v $(pwd):$(pwd) -w $(pwd) shaungc/terraform-kubectl-image <<EOL \
-    -backend-config="access_key=${TF_VAR_aws_access_key}" \
-    -backend-config="secret_key=${TF_VAR_aws_secret_key}" \
-    -backend-config="region=${TF_BACKEND_region}" \
+    terraform init -backend-config="access_key=${TF_VAR_aws_access_key}" \
+        -backend-config="secret_key=${TF_VAR_aws_secret_key}" \
+        -backend-config="region=${TF_BACKEND_region}" \
     && terraform validate \
     && terraform plan -var="app_container_image_tag=${CIRCLE_SHA1}" \
     && terraform apply -auto-approve -var="app_container_image_tag=${CIRCLE_SHA1}"
