59336c88229fe9f063fdb52f0d51342057d83ac1
Shaung <shaungc@umich.edu>
2020-02-19 00:07:09 -0800

    #19, commit inprogress work

--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,7 @@
 const expect  = require('chai').expect;
 const axios = require('axios').default;
 
-const server = require('./index');
+const server = require('./index').nodeServer;
 const baseUrl = require('./utilities/serverUtilities').baseUrl;
 
 
