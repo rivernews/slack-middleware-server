a40ebdf7a5b6a1faba72bd0af532d81795358ea1
Shaung Cheng <shaungc@umich.edu>
2020-04-02 23:26:41 -0700

    improve watch script

--- cicd/watch.sh
+++ cicd/watch.sh
@@ -7,7 +7,7 @@ while :;
   do 
   clear
   date
-  KUBECONFIG=kubeconfig.yaml kubectl -n redis-cluster exec deploy/redis-cluster-deployment -- bash -c "redis-cli <<<$'select 6\n keys sema*travis* \n zrange semaphore:travisJobResourceLock 0 -1 \n keys sema*k8* \n zrange semaphore:k8JobResourceLock 0 -1'"
+  KUBECONFIG=kubeconfig.yaml kubectl -n redis-cluster exec deploy/redis-cluster-deployment -- bash -c "redis-cli -a \$(printenv REDIS_PASSWORD) <<<$'select 6\n keys sema*travis* \n zrange semaphore:travisJobResourceLock 0 -1 \n keys sema*k8* \n zrange semaphore:k8JobResourceLock 0 -1'"
   sleep 3
 done
 
