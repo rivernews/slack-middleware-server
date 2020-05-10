f1c7e116a57366072dec257b84c9fd51a0f05845
Shaung Cheng <shaungc@umich.edu>
2020-05-10 09:17:06 +0000

    simplify s3 job workflow, remove redundant concurrency checks

--- src/JobQueueAPI/s3OrgsJob/process.ts
+++ src/JobQueueAPI/s3OrgsJob/process.ts
@@ -1,10 +1,8 @@
 import Bull from 'bull';
 import { s3ArchiveManager } from '../../services/s3';
 import { supervisorJobQueueManager } from '../supervisorJob/queue';
-import { s3OrgsJobQueueManager } from './queue';
 import { ProgressBarManager } from '../../services/jobQueue/ProgressBar';
 import { JobQueueName } from '../../services/jobQueue/jobQueueName';
-import { ServerError } from '../../utilities/serverExceptions';
 import {
     ScraperJobRequestData,
     ScraperMode,
@@ -61,200 +59,176 @@ module.exports = function (s3OrgsJob: Bull.Job<null>) {
     console.log(`s3OrgsJob ${s3OrgsJob.id} started`, s3OrgsJob);
 
     supervisorJobQueueManager.initialize(`s3Org sandbox`, false);
-    const supervisorJobQueueManagerQueue = supervisorJobQueueManager.queue;
-    if (!supervisorJobQueueManagerQueue) {
-        throw new ServerError(
-            `In s3Org sandbox process: supervisorJobQueueManager queue not initialized yet`
-        );
-    }
 
     const progressBarManager = ProgressBarManager.newProgressBarManager(
         JobQueueName.GD_ORG_REVIEW_S3_ORGS_JOB,
         s3OrgsJob
     );
 
-    return s3OrgsJobQueueManager
-        .checkConcurrency(
-            // check no existing s3 job running
-            1,
-            undefined,
-            s3OrgsJob,
-            JobQueueName.GD_ORG_REVIEW_S3_ORGS_JOB,
-            true
-        )
-        .then(() => {
-            // dispatch supervisors into parallel groups
-            // let Bull queue do the concurrency limiting
+    return (
+        s3ArchiveManager
+            .asyncGetAllOrgsForS3Job()
+            // increment progress after s3 org list fetched
+            .then(orgList =>
+                progressBarManager
+                    .increment()
+                    .then(() => {
+                        const supervisorJobRequests: SupervisorJobRequestData[] = [];
+                        for (const org of orgList) {
+                            // dispatch for large org (splitted job)
 
-            return (
-                s3ArchiveManager
-                    .asyncGetAllOrgsForS3Job()
-                    // increment progress after s3 org list fetched
-                    .then(orgList =>
-                        progressBarManager
-                            .increment()
-                            .then(() => {
-                                const supervisorJobRequests: SupervisorJobRequestData[] = [];
-                                for (const org of orgList) {
-                                    // dispatch for large org (splitted job)
-
-                                    if (
-                                        org.reviewPageUrl &&
-                                        org.localReviewCount &&
-                                        org.localReviewCount >
-                                            Configuration.singleton
-                                                .scraperJobSplittingSize
-                                    ) {
-                                        const incrementalPageAmount = Math.ceil(
-                                            Configuration.singleton
-                                                .scraperJobSplittingSize /
-                                                Configuration.singleton
-                                                    .gdReviewCountPerPage
-                                        );
-                                        let pageNumberPointer = 0;
-                                        let shardIndex = 0;
-
-                                        // dispatch 1st job
-                                        supervisorJobRequests.push({
-                                            splittedScraperJobRequestData: getSplittedJobRequestData(
-                                                org,
-                                                pageNumberPointer,
-                                                incrementalPageAmount,
-                                                shardIndex
-                                            )
-                                        });
-
-                                        pageNumberPointer += incrementalPageAmount;
-                                        shardIndex += 1;
-
-                                        // dispatch rest of the parts
-                                        const estimatedPageCountTotal = Math.ceil(
-                                            org.localReviewCount /
-                                                Configuration.singleton
-                                                    .gdReviewCountPerPage
-                                        );
-
-                                        while (
-                                            pageNumberPointer <
-                                            estimatedPageCountTotal
-                                        ) {
-                                            supervisorJobRequests.push({
-                                                splittedScraperJobRequestData: getSplittedJobRequestData(
-                                                    org,
-                                                    pageNumberPointer,
-                                                    incrementalPageAmount,
-                                                    shardIndex
-                                                )
-                                            });
-
-                                            pageNumberPointer += incrementalPageAmount;
-                                            shardIndex += 1;
-                                        }
-
-                                        // last splitted job does not need stop page, just scrape till the end
-                                        delete (supervisorJobRequests[
-                                            supervisorJobRequests.length - 1
-                                        ]
-                                            .splittedScraperJobRequestData as ScraperJobRequestData)
-                                            .stopPage;
-                                        // let last splitted job just use local review count instead of incremental amount
-                                        // since it may vary
-                                        ((supervisorJobRequests[
-                                            supervisorJobRequests.length - 1
-                                        ]
-                                            .splittedScraperJobRequestData as ScraperJobRequestData)
-                                            .lastProgress as ScraperProgressData).total =
-                                            org.localReviewCount;
-
-                                        continue;
-                                    }
-
-                                    // dispatch for small org that does not need splitting
-
-                                    supervisorJobRequests.push({
-                                        scraperJobRequestData: {
-                                            pubsubChannelName: getPubsubChannelName(
-                                                { orgName: org.orgName }
-                                            ),
-                                            orgInfo: org.companyOverviewPageUrl
-                                        }
-                                    });
-                                }
-
-                                // report progress after job planning complete
-                                if (!supervisorJobRequests.length) {
-                                    progressBarManager.syncSetAbsolutePercentage(
-                                        100
-                                    );
-                                } else {
-                                    progressBarManager.syncSetRelativePercentage(
-                                        0,
-                                        supervisorJobRequests.length
-                                    );
-                                }
-
-                                return supervisorJobRequests;
-                            })
-                            .then(scraperJobRequests => {
-                                return Promise.all(
-                                    scraperJobRequests.map(
-                                        (scraperJobRequest, index) =>
-                                            // supervisorJobQueueManager.asyncAdd(
-                                            //     scraperJobRequest
-                                            // )
-                                            new Promise(res =>
-                                                setTimeout(res, index * 1 * 500)
-                                            ).then(() => {
-                                                return supervisorJobQueueManager.asyncAdd(
-                                                    scraperJobRequest
-                                                );
-                                            })
-                                    )
+                            if (
+                                org.reviewPageUrl &&
+                                org.localReviewCount &&
+                                org.localReviewCount >
+                                    Configuration.singleton
+                                        .scraperJobSplittingSize
+                            ) {
+                                const incrementalPageAmount = Math.ceil(
+                                    Configuration.singleton
+                                        .scraperJobSplittingSize /
+                                        Configuration.singleton
+                                            .gdReviewCountPerPage
                                 );
-                            })
-                    )
-                    .then(supervisorJobList =>
-                        Promise.all(
-                            supervisorJobList.map(supervisorJob =>
-                                supervisorJob
-                                    .finished()
-                                    // increment progress after job finished, then propogate back job result
-                                    .then((result: string) =>
-                                        progressBarManager
-                                            .increment()
-                                            .then(() => result)
+                                let pageNumberPointer = 0;
+                                let shardIndex = 0;
+
+                                // dispatch 1st job
+                                supervisorJobRequests.push({
+                                    splittedScraperJobRequestData: getSplittedJobRequestData(
+                                        org,
+                                        pageNumberPointer,
+                                        incrementalPageAmount,
+                                        shardIndex
                                     )
+                                });
+
+                                pageNumberPointer += incrementalPageAmount;
+                                shardIndex += 1;
+
+                                // dispatch rest of the parts
+                                const estimatedPageCountTotal = Math.ceil(
+                                    org.localReviewCount /
+                                        Configuration.singleton
+                                            .gdReviewCountPerPage
+                                );
+
+                                while (
+                                    pageNumberPointer < estimatedPageCountTotal
+                                ) {
+                                    supervisorJobRequests.push({
+                                        splittedScraperJobRequestData: getSplittedJobRequestData(
+                                            org,
+                                            pageNumberPointer,
+                                            incrementalPageAmount,
+                                            shardIndex
+                                        )
+                                    });
+
+                                    pageNumberPointer += incrementalPageAmount;
+                                    shardIndex += 1;
+                                }
+
+                                // last splitted job does not need stop page, just scrape till the end
+                                delete (supervisorJobRequests[
+                                    supervisorJobRequests.length - 1
+                                ]
+                                    .splittedScraperJobRequestData as ScraperJobRequestData)
+                                    .stopPage;
+                                // let last splitted job just use local review count instead of incremental amount
+                                // since it may vary
+                                ((supervisorJobRequests[
+                                    supervisorJobRequests.length - 1
+                                ]
+                                    .splittedScraperJobRequestData as ScraperJobRequestData)
+                                    .lastProgress as ScraperProgressData).total =
+                                    org.localReviewCount;
+
+                                continue;
+                            }
+
+                            // dispatch for small org that does not need splitting
+
+                            supervisorJobRequests.push({
+                                scraperJobRequestData: {
+                                    pubsubChannelName: getPubsubChannelName({
+                                        orgName: org.orgName
+                                    }),
+                                    orgInfo: org.companyOverviewPageUrl
+                                }
+                            });
+                        }
+
+                        // report progress after job planning complete
+                        if (!supervisorJobRequests.length) {
+                            progressBarManager.syncSetAbsolutePercentage(100);
+                        } else {
+                            progressBarManager.syncSetRelativePercentage(
+                                0,
+                                supervisorJobRequests.length
+                            );
+                        }
+
+                        return supervisorJobRequests;
+                    })
+                    .then(scraperJobRequests => {
+                        return Promise.all(
+                            scraperJobRequests.map((scraperJobRequest, index) =>
+                                // supervisorJobQueueManager.asyncAdd(
+                                //     scraperJobRequest
+                                // )
+                                new Promise(res =>
+                                    setTimeout(res, index * 1 * 600)
+                                ).then(() => {
+                                    return supervisorJobQueueManager.asyncAdd(
+                                        scraperJobRequest
+                                    );
+                                })
                             )
-                        )
+                        );
+                    })
+            )
+            .then(supervisorJobList =>
+                Promise.all(
+                    supervisorJobList.map(supervisorJob =>
+                        supervisorJob
+                            .finished()
+                            // increment progress after job finished, then propogate back job result
+                            .then((result: string) =>
+                                progressBarManager
+                                    .increment()
+                                    .then(() => result)
+                            )
                     )
-            );
-        })
-        .then((resultList: string[]) => Promise.resolve(resultList))
-        .catch((error: Error) => Promise.reject(error))
-        .finally(async () => {
-            console.log('cool down for 10 seconds');
-            await new Promise(res => setTimeout(res, 10 * 1000));
+                )
+            )
+            .then((resultList: string[]) => Promise.resolve(resultList))
+            .catch((error: Error) => Promise.reject(error))
+            .finally(async () => {
+                console.log('cool down for 30 seconds');
+                await new Promise(res => setTimeout(res, 30 * 1000));
 
-            let scaledownError;
-            try {
-                await ScraperNodeScaler.singleton.orderScaleDown();
+                let scaledownError;
+                try {
+                    await ScraperNodeScaler.singleton.orderScaleDown();
 
-                console.log('cool down for 10 seconds');
-                await new Promise(res => setTimeout(res, 10 * 1000));
+                    console.log('cool down for 20 seconds');
+                    await new Promise(res => setTimeout(res, 20 * 1000));
 
-                await KubernetesService.singleton._cleanScraperWorkerNodePools();
-            } catch (error) {
-                console.log(error);
-                scaledownError = error;
-            }
-            const slackRes = await asyncSendSlackMessage(
-                `S3 work done, best effort scaled down selenium microservice:\n\n\n\`\`\`${
-                    scaledownError instanceof Error
-                        ? scaledownError.message
-                        : JSON.stringify(scaledownError)
-                }\`\`\``
-            );
-            console.log(slackRes.data, slackRes.statusText);
-            return await supervisorJobQueueManager.asyncCleanUp();
-        });
+                    await KubernetesService.singleton._cleanScraperWorkerNodePools();
+                } catch (error) {
+                    console.log(error);
+                    scaledownError = error;
+                }
+                const slackRes = await asyncSendSlackMessage(
+                    `S3 work done, best effort scaled down selenium microservice:\n\n\n\`\`\`${
+                        scaledownError instanceof Error
+                            ? scaledownError.message
+                            : JSON.stringify(scaledownError)
+                    }\`\`\``
+                );
+                console.log(slackRes.data, slackRes.statusText);
+                return await supervisorJobQueueManager.asyncCleanUp();
+            })
+    );
 };
