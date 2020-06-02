dbf46ed6b8f0fc24a7e8bb52066c297bf06e0b22
Shaung Cheng <shaungc@umich.edu>
2020-06-02 00:34:10 +0000

    establish TODO items and document resources to look for

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,3 +1,21 @@
-import { createLogger } from 'winston';
+import { createLogger, format } from 'winston';
+import { Configuration } from './configuration';
 
-const logger = createLogger({});
+// winston logger
+// https://github.com/winstonjs/winston#creating-your-own-logger
+
+export const Logger = createLogger({
+    level: Configuration.singleton.logLevel,
+    format: format.simple(),
+    defaultMeta: {
+        pid: process.pid
+    }
+});
+
+// TODO: add prefix like 🔴 🟠 ... --> format
+
+// TODO: integrate with opt-in slack message --> transport? + level?
+// slack transport! https://github.com/winstonjs/winston/blob/master/docs/transports.md#slack-transport
+
+// TODO: being able to add to redis --> transport?
+// redis transport: https://github.com/winstonjs/winston/blob/master/docs/transports.md#redis-transport
